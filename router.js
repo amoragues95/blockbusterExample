4d8228240b2beb7cb155788475fbf4c5e719583d
Kevin Garces Høyberg <68248803+kevinghberg@users.noreply.github.com>
2022-10-03 11:37:42 -0300

    add: movie endpoint

--- router.js
+++ router.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const movieController = require('./controllers/movieController')
 const router = express.Router();
 const bodyParser = require('body-parser');
 
 
 router.use(bodyParser.json())
+router.use(express.json());
+router.use(express.urlencoded({extended: true}))
+
+router.get('/movies',movieController.getMovies);
 
 module.exports = router;
