7f6952dee2e7aeee954a280bf9953682ce9804c4
unknown <amoragues95@gmail.com>
2022-10-03 11:00:42 -0300

    add comment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const router = require('./router')
 require("dotenv").config()
 const app = express()
 const port = 3000
-
+//prueba
 app.use('/', router);
 const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
