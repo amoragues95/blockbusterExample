753c74df1a23d5037380ea96f4aa727b2521b883
AgusBaez <agustinbaezignacio@gmail.com>
2022-10-03 10:52:50 -0300

    :tada: Initial commit

--- config/config.js
+++ config/config.js
@@ -6,20 +6,20 @@ module.exports = {
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
     "host": "localhost",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "test": {
     "username":process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_TEST_NAME,
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 }
