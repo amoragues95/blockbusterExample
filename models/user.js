d228958484b023d9d99a521a54a3550207fdbb2d
AgusBaez <agustinbaezignacio@gmail.com>
2022-10-03 11:29:45 -0300

    :sparkles: Create User Model

---
+++ models/user.js
@@ -0,0 +1,41 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      dni: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      phone: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
